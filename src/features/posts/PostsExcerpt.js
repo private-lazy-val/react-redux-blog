7bf76f4798098430e670d409a6ef313a4b5d416d
Valeriya Evstratova <valelondonenko@yahoo.com>
2023-10-16 21:07:18 +0400

    update css and readme

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -11,7 +11,7 @@ const PostsExcerpt = ({postId}) => {
     return (
         <article>
             <h4>{post.title}</h4>
-            <p className='excerpt'>{post.body.substring(0, 75)}</p>
+            <p>{post.body.substring(0, 75)}</p>
             <p className="postCredit">
                 <Link to={`post/${post.id}`}>View Post</Link>
                 <PostAuthor userId={post.userId} />
